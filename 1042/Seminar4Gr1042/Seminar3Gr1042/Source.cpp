57a93c689f542e03bac09c30ffad51a2a6fae3d4
irinaenachescu <irina.enachescu@csie.ase.ro>
2016-10-26 18:32:38 +0300

    Add files via upload

---
+++ 1042/Seminar4Gr1042/Seminar3Gr1042/Source.cpp
@@ -0,0 +1,192 @@
+#include<iostream>
+using namespace std;
+class Carte
+{
+private:
+	const int id_carte;
+	char* autor;
+	char* titlu;
+	char gen[50];
+	unsigned int numar_pagini;
+	double pret;
+	static unsigned int numar_carti;
+public:
+	Carte() :id_carte(Carte::numar_carti++)
+	{
+		this->autor = new char[strlen("Anonim") + 1];
+		strcpy(this->autor, "Anonim");
+		this->titlu = new char[strlen("Lipsa") + 1];
+		strcpy(this->titlu, "Lipsa");
+		strcpy(this->gen, "Lipsa");
+		this->numar_pagini = 0;
+		this->pret = 0;
+	}
+
+	Carte(char* autor, char* titlu, char* gen, unsigned int numar_pagini, double pret) :id_carte(Carte::numar_carti++)
+	{
+		this->autor = new char[strlen(autor) + 1];
+		strcpy(this->autor, autor);
+
+		this->titlu = new char[strlen(titlu) + 1];
+		strcpy(this->titlu, titlu);
+
+		strcpy(this->gen, gen);
+		this->numar_pagini = numar_pagini;
+		this->pret = pret;
+	}
+
+
+	Carte(char* titlu, char* autor, char* gen, unsigned int nr_pagini = 50) :numar_pagini(nr_pagini), id_carte(Carte::numar_carti++)
+	{
+		this->autor = new char[strlen(autor) + 1];
+		strcpy(this->autor, autor);
+
+		this->titlu = new char[strlen(titlu) + 1];
+		strcpy(this->titlu, titlu);
+
+		strcpy(this->gen, gen);
+		this->pret = 100.0;
+
+	}
+	void afisare()
+	{
+		cout << "Id: " << this->id_carte << endl;
+		cout << "Titlu: " << this->titlu << endl;
+		cout << "Autor: " << this->autor << endl;
+		cout << "Gen: " << this->gen << endl;
+		cout << "Numar pagini: " << this->numar_pagini << endl;
+		cout << "Pret: " << this->pret << endl;
+	}
+
+	~Carte()
+	{
+		cout << "In destructor" << endl;
+		delete[] this->autor;
+		delete[] this->titlu;
+	}
+
+	void setAutor(char* autor2)
+	{
+		if (this->autor != NULL)
+		{
+			delete[] this->autor;
+		}
+		this->autor = new char[strlen(autor2) + 1];
+		strcpy(this->autor, autor2);
+	}
+
+	char* getAutor()
+	{
+		return this->autor;
+	}
+
+	void setTitlu(char* titlu)
+	{
+		if (this->titlu != NULL)
+		{
+			delete[] this->titlu;
+		}
+		if (strlen(titlu) >= 3)
+		{
+			this->titlu = new char[strlen(titlu) + 1];
+			strcpy(this->titlu, titlu);
+		}
+		else
+		{
+			throw new exception("Titlu prea scurt!");
+		}
+	}
+
+	char* getTitlu()
+	{
+		return this->titlu;
+	}
+
+	void setGen(char* gen)
+	{
+		strcpy(this->gen, gen);
+	}
+
+	char* getGen()
+	{
+		return this->gen;
+	}
+
+	unsigned int getNrPagini()
+	{
+		return this->numar_pagini;
+	}
+
+	void setNrPagini(unsigned int numar_pagini)
+	{
+		this->numar_pagini = numar_pagini;
+	}
+
+	double getPret()
+	{
+		return this->pret;
+	}
+
+	void setPret(double pret);
+
+	Carte(const Carte& carte) :id_carte(carte.id_carte)
+	{
+		this->autor = new char[strlen(carte.autor) + 1];
+		strcpy(this->autor, carte.autor);
+
+		this->titlu = new char[strlen(carte.titlu) + 1];
+		strcpy(this->titlu, carte.titlu);
+
+		strcpy(this->gen, carte.gen);
+		this->numar_pagini = carte.numar_pagini;
+		this->pret = carte.pret;
+	}
+
+};
+
+unsigned int Carte::numar_carti = 0;
+
+void Carte::setPret(double pret)
+{
+	this->pret = pret;
+}
+
+void main()
+{
+	Carte carte1;
+	carte1.afisare();
+
+	Carte* p_carte1 = new Carte();
+	p_carte1->afisare();
+	delete p_carte1;
+
+	Carte carte2("Liviu Rebreanu", "Ion", "roman interbelic", 100, 150.5);
+	carte2.afisare();
+
+	carte2.setAutor("Liviu R");
+	carte2.setTitlu("Ionel");
+	carte2.setGen("roman");
+	carte2.setNrPagini(50);
+	carte2.setPret(55.6);
+	cout << carte2.getAutor() << endl;
+	cout << carte2.getTitlu() << endl;
+	cout << carte2.getGen() << endl;
+	cout << carte2.getNrPagini() << endl;
+	cout << carte2.getPret() << endl;
+
+	//apelul constr de copiere
+	Carte carte5 = carte2;
+	carte5.afisare();
+
+	Carte* p_carte2= new Carte("Camil Petrescu", "Ultima noapte...", "roman interbelic", 200, 250.5);
+	p_carte2->afisare();
+	delete p_carte2;
+
+	Carte carte3("Morometii 2", "Marin Preda", "roman postbelic");
+	carte3.afisare();
+
+	Carte carte4("Morometii 2","Marin Preda", "roman postbelic", 500);
+	carte4.afisare();
+	carte4.setAutor("Ion Ion");
+	carte4.afisare();
+}
